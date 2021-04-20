0f8b0c811232cd3de0cf997f448a4b6632e771f6
Sei <iamsei97@gmail.com>
2021-04-20 11:34:11 +0200

    Adding DataAccess & References

--- DataAccess/DataAccess.cs
+++ DataAccess/DataAccess.cs
@@ -1,6 +1,8 @@
 ﻿using CommonTypes;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +16,24 @@ namespace DataAccess
         public void SaveKFZ(KFZ kfz)
         {
             MessageBox.Show($"Data-Access SaveKFZ {kfz.Kennzeichen}");
-            //In Azure-DB speichern.
+
+            //Verbindung zur DB aufbauen.
+            MySqlConnection myConnection = new SqlConnection("SERVER=localhost;DATABASE=;UID=root;PASSWORD=;");
+            myConnection.Open();
+
+            //Einfüge SQL-Befehl zusammenbauen.
+            string myInsertQuery = "INSERT INTO t_chatquote (alias, quote, timestamp) Values('""')";
+            MySqlCommand myCommand = new MySqlCommand(myInsertQuery);
+
+            //Dem SQL-Befehl noch sagen, welche Verbindung zum Server verwendet werden soll.
+            myCommand.Connection = myConnection;
+
+            //Befehl ausführen.
+            myCommand.ExecuteNonQuery();
+
+            //Verbindung zur Datenbank wieder abbauen.
+            myConnection.Close();
+
         }
     }
 }
